d7473ea9468a1fe2bf58fdc7bca6e9cab68c51a9
TPosty <152321491+TPosty@users.noreply.github.com>
2026-01-22 08:37:25 -0600

    Started working on saving projects to local storage

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import Project from "./project.js";
 
 const create_project_button = document.querySelector(".create_project_button");
 const project_name_text = document.querySelector(".project_name_text");
-const project_instance = new Project();
 
 create_project_button.addEventListener("click", () => {
     const project_name = project_name_text.value;
-    project_instance.create_project(project_name);
+    const project_instance = new Project(project_name);
+
+    const new_project_json = project_instance.create_project();
+
+    console.log(project_instance.add_todo());
+
+    localStorage.setItem(
+        "Projects",
+        JSON.stringify([project_instance.create_project()])
+    );
 });
--- src/project.js
+++ src/project.js
@@ -1,15 +1,31 @@
 const project_container = document.querySelector(".project_container");
 
 export default class Project {
-    // constructor(project_name) {
-    //     this.project_name = project_name;
-    // }
+    constructor(project_name) {
+        this.project_name = project_name;
+        this.project_id = crypto.randomUUID();
+        this.todos = [];
+    }
 
-    create_project(project_name) {
+    create_project() {
         const project_button = document.createElement("button");
-        project_button.textContent = project_name;
+        project_button.textContent = this.project_name;
         project_button.classList.add("project_button");
 
+        // Should the project class really be updating the display? Probaby not...
         project_container.appendChild(project_button);
+
+        return {
+            name: this.project_name,
+            id: this.project_id,
+            todos: this.todos,
+        };
+    }
+
+    add_todo() {
+        this.todos.push({
+            name: "testing",
+            id: crypto.randomUUID(),
+        });
     }
 }
